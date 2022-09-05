0b963f9fda316581a49f7a5cb39f9e45718f9bba
CHEREAL <mc.cherdak@mail.ru>
2022-09-05 23:28:13 +0300

    the end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* Corousel*/
+
 $(document).ready(function(){
   $(".owl-carousel").owlCarousel({
     center : true,
@@ -5,6 +7,7 @@ $(document).ready(function(){
     margin: 30,
     startPosition: 1,
     items: 3,
+    
   });
 });
 
@@ -12,9 +15,26 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({    
   center : true,
   loop : true,
-  margin: 30,
+  margin: 20,
   startPosition: 1,
   items: 3,
+  responsive : {
+   
+    340 : {
+      startPosition: 1,
+    items: 1,
+    },
+   
+    850 : {
+      startPosition: 1,
+    items: 3,
+    },
+  
+    1200 : {
+      items: 3,
+      margin: 30,
+    },
+},
 });
 
 $('.slider__btn--prev').click(function() {
@@ -24,4 +44,18 @@ $('.slider__btn--prev').click(function() {
 $('.slider__btn--next').click(function() {
     owl.trigger('next.owl.carousel');
 })
+
+/* Nav menu*/
+
+const navBtn = document.querySelector('.nav__toggle');
+const menuIcon = document.querySelector('.menu-icon');
+const nav = document.querySelector('.nav');
+
+
+navBtn.onclick = function () {
+  nav.classList.toggle('nav--mobile');
+  menuIcon.classList.toggle('menu-icon-active');
+  document.body.classList.toggle('no-scroll');
+};
+
 
